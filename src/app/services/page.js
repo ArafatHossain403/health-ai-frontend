31bbf71a3b90ea97178782fe63ff0c6ca14a27fa
rukaiya22 <rukaiyarafique2000@gmail.com>
2023-12-15 19:26:10 +0600

    signup login service page setup

---
+++ src/app/services/page.js
@@ -0,0 +1,119 @@
+import Link from 'next/link';
+import React from 'react';
+
+const ServicesPage = () => {
+  return (
+    <div>
+        <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          
+         
+          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form className="card-body">
+            <h1 className='text-center text-lg font-bold'>
+            Diabetes Prediction Form
+          </h1>
+          <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Pregnancis</span>
+                </label>
+                <input
+                  type="tel"
+                  placeholder="number"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Glucose</span>
+                </label>
+                <input
+                  type="tel"
+                  placeholder="number"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Blood Pressure</span>
+                </label>
+                <input
+                  type="tel"
+                  placeholder="number"
+                  className="input input-bordered"
+                  required
+                />
+                <label className="label">
+                  
+                </label>
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Skin Thickness</span>
+                </label>
+                <input
+                  type="tel"
+                  placeholder="number"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Insulin</span>
+                </label>
+                <input
+                  type="tel"
+                  placeholder="number"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">BMI</span>
+                </label>
+                <input
+                  type="tel"
+                  placeholder="number"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Diabetes Pedigree Function</span>
+                </label>
+                <input
+                  type="tel"
+                  placeholder="number"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Age</span>
+                </label>
+                <input
+                  type="tel"
+                  placeholder="number"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Submit</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ServicesPage;
--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,12 +1,9 @@
 import Link from 'next/link';
 import React from 'react';
 
-const LoginPage = () => {
-    return (
-        <div>
-      
-     
-
+const SignupPage = () => {
+  return (
+    <div>
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row-reverse">
           
@@ -14,8 +11,20 @@ const LoginPage = () => {
           <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
             <form className="card-body">
             <h1 className='text-center text-lg font-bold'>
-            Please Login
+            Please Signup
           </h1>
+          <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="name"
+                  placeholder="name"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
@@ -43,25 +52,48 @@ const LoginPage = () => {
                   </a>
                 </label>
               </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Mobile</span>
+                </label>
+                <input
+                  type="tel"
+                  placeholder="number"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Address</span>
+                </label>
+                <input
+                  type="tel"
+                  placeholder="Address"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+
               <div>
                 <label className="label">
                   <a href="#" className="label-text-alt link link-hover">
-                  Do not Have an Account?
+                  Do Have an Account?
                   </a>
                   
                 </label>
-                <Link className="link link-accent" href="/signup">Signup</Link>
+                <Link className="link link-accent" href="/login">Login</Link>
                 
               </div>
               <div className="form-control mt-6">
-                <button className="btn btn-primary">Login</button>
+                <button className="btn btn-primary">Signup</button>
               </div>
             </form>
           </div>
         </div>
       </div>
     </div>
-    );
+  );
 };
 
-export default LoginPage;
+export default SignupPage;
