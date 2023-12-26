d81cadac3b979013fd12331622a0c79300dd38c3
Dipankar <dipankarsaha1059@gmail.com>
2023-12-26 13:46:34 +0600

    slash cleaning fn updated

--- src/app/helper/functions.js
+++ src/app/helper/functions.js
@@ -1,11 +1,20 @@
 export const getApiUrl = () => {
-    let url = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000//';
-    // clean url...
+    let url = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+    url = cleanEndSlash(url);
     return url;
 }
 
+export function cleanMultiSlash(endpoint, replaceWith = '/') {
+    endpoint = endpoint.replace(/\/{2,}/g, replaceWith);
+    return endpoint;
+}
+
 export function cleanStartingSlash(endpoint) {
-    endpoint = endpoint.replace(/\/{2,}/, '/');
     endpoint = endpoint.startsWith('/') ? endpoint.substring(1) : endpoint;
+    return endpoint;
+}
+
+export function cleanEndSlash(endpoint) {
+    endpoint = endpoint.endsWith('/') ? endpoint.substring(0, endpoint.length - 1) : endpoint;
     return endpoint;
 }
